c19bd496267590616be7dc7525d9c8396d6733a9
szadyraka <sergey.zadiraka@gmail.com>
2019-10-21 11:19:57 +0100

    Try with resources https://www.baeldung.com/java-try-with-resources

---
+++ src/com/baeldung/java/core/TryWithResources.java
@@ -0,0 +1,45 @@
+package com.baeldung.java.core;
+
+import java.io.*;
+import java.util.Scanner;
+
+public class TryWithResources {
+    public static void main(String[] args) {
+        createTestData();
+        //Reading from the file using try without resources
+        Scanner scanner = null;
+        try {
+            scanner = new Scanner(new File("test.txt"));
+            System.out.println("Reading from the file using \"try without resources\": ");
+            while (scanner.hasNext()) {
+                System.out.println(scanner.nextLine());
+            }
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } finally {
+            if (scanner != null) {
+                scanner.close();
+            }
+        }
+        //Reading from the file using try without resources
+        try (Scanner sc = new Scanner(new File("test.txt"))) {
+            System.out.println("Reading from the file using \"try with resources\": ");
+            while (sc.hasNext()) {
+                System.out.println(sc.nextLine());
+            }
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static void createTestData(){
+        try {
+            BufferedWriter writer = new BufferedWriter(new FileWriter("test.txt"));
+            writer.write("test data line1\n");
+            writer.write("test data line2\n");
+            writer.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
